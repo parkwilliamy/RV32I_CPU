4bf530a3650243af4e0da45e951aef9c36c4a6b0
William Park <parkwilliamy@gmail.com>
2025-09-30 18:06:13 -0400

    finished CRV tests of ImmGen, will verify RegFile next

--- rtl/units/ImmGen.v
+++ rtl/units/ImmGen.v
@@ -1,36 +1,67 @@
+`timescale 1ns/1ps
+
 module ImmGen (
     input [31:0] instruction,
-    output [31:0] eximm;
+    output reg [11:0] imm1,
+    output reg [19:0] imm2,
+    output reg [31:0] eximm
 );
 
-    assign wire [6:0] opcode = instruction[6:0];
-    wire [11:0] imm;
+    reg [6:0] opcode;
+    reg [11:0] intimm1;
+    reg [19:0] intimm2;
+    reg [31:0] eximm1;
+    reg [31:0] eximm2;
     
     localparam [6:0] // opcodes for different instruction types
-        OP      = 7'b0110011;
-        OP_I  = 7'b0010011,
+        OP_R = 7'b0110011,
+        OP_I = 7'b0010011,
         OP_S = 7'b0100011,
         OP_B = 7'b1100011,
-        OP_U = 7'0110111,
-        OP_J = 7'1101111;
+        OP_U = 7'b0110111,
+        OP_J = 7'b1101111;
 
     always @(*) begin
 
+        opcode = instruction[6:0];
+
         case(opcode)
 
-            OP_I: imm = instruction[31:20];
-            OP_S: imm = {instruction[31:25], instruction[11:7]};
-            OP_B: imm = {instruction[31], instruction[7], instruction[30:25], instruction[11:8]};
-            OP_U: imm = instruction[31:12];
-            OP_J: imm = {instruction[31], instruction[19:12], instruction[20], instruction[30:21]};
-            default: imm = 0; // only for R-type format
+            OP_I: begin
+                intimm1 = instruction[31:20];
+                intimm2 = 0;
+            end
+            OP_S: begin
+                intimm1 = {instruction[31:25], instruction[11:7]};
+                intimm2 = 0;
+            end
+            OP_B: begin
+                intimm1 = {instruction[31], instruction[7], instruction[30:25], instruction[11:8]};
+                intimm2 = 0;
+            end
+            OP_U: begin
+                intimm1 = 0;
+                intimm2 = instruction[31:12];
+            end
+            OP_J: begin
+                intimm1 = 0;
+                intimm2 = {instruction[31], instruction[19:12], instruction[20], instruction[30:21]};
+            end
+            default: begin 
+                intimm1 = 0; // only for R-type format
+                intimm2 = 0;
+            end
 
         endcase
 
+    imm1 = intimm1;
+    imm2 = intimm2;
+    eximm1 = {{20{intimm1[11]}}, intimm1};
+    eximm2 = {{12{intimm2[19]}}, intimm2};
+    eximm = (opcode == OP_U || opcode == OP_J) ? eximm2 : eximm1; // sign extend by 12 bits for U and J-type formats, extend by 20 bits for other formats
+
     end
 
-    assign exnum = (opcode == OP_U || opcode == OP_J) ? 12 : 20; // sign extend by 12 bits for U and J-type formats, extend by 20 bits for other formats
-    assign eximm = {{exnum{imm[exnum-1]}}, imm}; 
-
+    
 
 endmodule
---
+++ tb/units/ImmGen_tb.sv
@@ -0,0 +1,55 @@
+`timescale 1ns/1ps
+
+class ImmGeninputs;
+
+    rand logic [31:7] upperinst; // non opcode part of instruction
+
+endclass
+
+module ImmGen_tb;
+
+    logic [31:0] instruction;
+    logic signed [11:0] imm1;
+    logic signed [19:0] imm2;
+    logic signed [31:0] eximm;
+    ImmGen DUT(.instruction(instruction), .imm1(imm1), .imm2(imm2), .eximm(eximm));
+    ImmGeninputs ImmGenTest = new;
+
+    localparam [6:0] // opcodes for different instruction types
+        OP_R = 7'b0110011,
+        OP_I = 7'b0010011,
+        OP_S = 7'b0100011,
+        OP_B = 7'b1100011,
+        OP_U = 7'b0110111,
+        OP_J = 7'b1101111;
+
+    logic [0:5][6:0] opcodes = '{OP_R, OP_I, OP_S, OP_B, OP_U, OP_J};
+
+    initial begin
+
+        $display("Testbench started!");
+
+        for (int i = 0; i < 6; i++) begin
+
+            $display("Opcode: %0b", opcodes[i]);
+
+            repeat(5) begin
+
+                ImmGenTest.randomize();
+                instruction = {ImmGenTest.upperinst, opcodes[i]};
+                #5;
+                $display("Instruction: %0b, imm1: %0d, imm2: %0d, eximm: %0d", instruction, imm1, imm2, eximm);
+
+            end
+
+            $display("=============================================");
+
+        end
+
+    $finish;
+
+    end
+
+    
+
+endmodule
