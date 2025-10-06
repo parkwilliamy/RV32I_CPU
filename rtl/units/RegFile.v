ac132a1559c5e015a5745f23ce18a730b89ba122
William Park <parkwilliamy@gmail.com>
2025-10-06 09:57:55 -0400

    still working on regfile tb

--- rtl/units/RegFile.v
+++ rtl/units/RegFile.v
@@ -1,30 +1,31 @@
+`timescale 1ns/1ps
+
 module RegFile (
     input clk, reg_write,
     input [4:0] rs1, rs2, rd,
     input [31:0] rd_data,
-    //input [1023:0] reg_file_flat, //for testing purposes
-    input [31:0] reg_file [31:0],
     output [31:0] rs1_data, rs2_data
 );
 
-    //reg [31:0] reg_file [31:0];
+    reg [31:0] reg_file [31:0];
+    integer i;
     
     assign rs1_data = reg_file[rs1];
     assign rs2_data = reg_file[rs2];
-/*
+
     always @(*) begin
 
-        for (int i = 0; i < 32; i++) begin
+        for (i = 0; i < 32; i = i+1) begin
         
-            reg_file[i] = reg_file_flat[32*i+31:32*i]; 
+            reg_file[i] = i;
 
         end
 
     end
-*/
+
     always @(posedge clk) begin
 
-        //reg_file[0] <= 0; // x0 hardwired to 0
+        reg_file[0] <= 0; // x0 hardwired to 0
         if ((rd > 0 && rd <= 31) && reg_write) reg_file[rd] <= rd_data; // ensure address written to is from 1-31 and reg_write is HIGH
         
     end
--- tb/units/RegFile_tb.sv
+++ tb/units/RegFile_tb.sv
@@ -4,29 +4,23 @@ class RegFileInputs;
 
     rand logic [4:0] rs1, rs2, rd;
     rand logic [31:0] rd_data;
-    rand logic [31:0] reg_file [31:0];
 
-    constraint addr_space = {
+    constraint addr_space {
         rs1 inside {[0:31]};
         rs2 inside {[0:31]};
         rd inside {[0:31]};
     }
 
-    constraint reg_file_data = {
-        reg_file[0] = 0; //x0 hardwired to 0
-    }
-
 endclass
 
-module RegFile_tb
+module RegFile_tb;
 
     bit clk, reg_write;
     logic [4:0] rs1, rs2, rd;
     logic [31:0] rd_data;
-    logic [31:0] reg_file [31:0];
     logic [31:0] rs1_data, rs2_data;
 
-    RegFile DUT(.clk(clk), .rs1(rs1), .rs2(rs2), .rd(rd), .rd_data(rd_data), .reg_file(reg_file), .rs1_data(rs1_data), .rs2_data(rs2_data));
+    RegFile DUT(.clk(clk), .reg_write(reg_write), .rs1(rs1), .rs2(rs2), .rd(rd), .rd_data(rd_data), .rs1_data(rs1_data), .rs2_data(rs2_data));
     RegFileInputs RegFileTest = new;
 
     always #10 clk = ~clk;
@@ -42,13 +36,8 @@ module RegFile_tb
             RegFileTest.randomize();
             rs1 = RegFileTest.rs1;
             rs2 = RegFileTest.rs2;
-            reg_file = RegFileTest.reg_file;
             #2;
-            $display("RegFile:");
-            for (int i = 0; i < 32; i++) begin
-                $display("%0h", reg_file[i]);
-            end;
-            $display("rs1: %0d, rs2: %0d", rs1, rs2);
+            $display("rs1: %0d, rs2: %0d, rs1_data: %0d, rs2_data: %0d", rs1, rs2, rs1_data, rs2_data);
 
         end
 
