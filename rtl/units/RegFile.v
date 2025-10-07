3f80309e59a32fec9db8ad3b62086b661f8ee95f
William Park <parkwilliamy@gmail.com>
2025-10-06 21:10:13 -0400

    almost finished regfile tb, need to debug w waveforms

--- rtl/units/RegFile.v
+++ rtl/units/RegFile.v
@@ -3,8 +3,8 @@
 module RegFile (
     input clk, reg_write,
     input [4:0] rs1, rs2, rd,
-    input [31:0] rd_data,
-    output [31:0] rs1_data, rs2_data
+    input [31:0] rd_write_data,
+    output [31:0] rs1_data, rs2_data, rd_data // rd_data purely for testing, to see value before and after clock edge
 );
 
     reg [31:0] reg_file [31:0];
@@ -12,10 +12,13 @@ module RegFile (
     
     assign rs1_data = reg_file[rs1];
     assign rs2_data = reg_file[rs2];
+    assign rd_data = reg_file[rd];
 
-    always @(*) begin
+    initial begin
 
-        for (i = 0; i < 32; i = i+1) begin
+        reg_file[0] <= 0; // x0 hardwired to 0
+
+        for (i = 1; i < 32; i = i+1) begin
         
             reg_file[i] = i;
 
@@ -25,8 +28,7 @@ module RegFile (
 
     always @(posedge clk) begin
 
-        reg_file[0] <= 0; // x0 hardwired to 0
-        if ((rd > 0 && rd <= 31) && reg_write) reg_file[rd] <= rd_data; // ensure address written to is from 1-31 and reg_write is HIGH
+        if ((rd > 0 && rd <= 31) && reg_write) reg_file[rd] <= rd_write_data; // ensure address written to is from 1-31 and reg_write is HIGH
         
     end
     
