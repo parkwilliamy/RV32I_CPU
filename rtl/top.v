c0369414c5beef40337479bcd0ab545af7fba700
parkwilliamy <parkwilliamy@gmail.com>
2025-11-15 00:06:22 -0500

    began pipeline design

--- rtl/top.v
+++ rtl/top.v
@@ -24,10 +24,25 @@ module top (
 
     reg [7:0] mem [IMEM_START:DMEM_END-1]; 
 
-               
-    // =============================== INSTRUCTION FIELDS ===============================
 
-    reg [31:0] instruction = 0;
+    // ================================ PIPELINE REGISTERS ==============================
+
+    // TODO: FIGURE OUT DUAL PORT BRAM IP BLOCK
+
+    reg [63:0] IF/ID; // IF/ID[63:32] = pc, IF/ID[31:0] = instruction
+    reg [132:0] ID/EX; // ID/EX[132:101] = pc, ID/EX[100:69] = rs1_data, ID/EX[68:37] = rs2_data, ID/EX[36:5] = eximm, ID/EX[4:0] = rd
+    reg [104:0] EX/MEM; // EX/MEM[104:73] = pc+eximm, EX/MEM[72:41] = rs2_data, EX/MEM[40:9] = alu_result, EX/MEM[8] = zero, EX/MEM[7] = sign, EX/MEM[6] = overflow, EX/MEM[5] = carry, EX/MEM[4:0] = rd
+    reg [68:0] MEM/WB; // MEM/WB[68:37] = alu_result, MEM/WB[36:5] = mem[alu_result], MEM/WB[4:0] = rd
+
+               
+    // =============================== INSTRUCTION FETCH ================================
+
+    reg [31:0] instruction;
+
+
+    // =============================== INSTRUCTION DECODE ===============================
+
+    wire [31:0] ID_instruction;
     wire [6:0] opcode;
     wire [11:7] rd;
     wire [14:12] funct3;
@@ -35,38 +50,23 @@ module top (
     wire [24:20] rs2;
     wire [31:25] funct7;
     
-    assign opcode = instruction[6:0];
-    assign rd = instruction[11:7];
-    assign funct3 = instruction[14:12];
-    assign rs1 = instruction[19:15];
-    assign rs2 = instruction[24:20];
-    assign funct7 = instruction[31:25];
+    assign ID_instruction = IF/ID[31:0]
+    assign opcode = ID_instruction[6:0];
+    assign rd = ID_instruction[11:7];
+    assign funct3 = ID_instruction[14:12];
+    assign rs1 = ID_instruction[19:15];
+    assign rs2 = ID_instruction[24:20];
+    assign funct7 = ID_instruction[31:25];
 
-
-    // ============================== CONTROL FIELDS/FLAGS ==============================
-
-    wire [1:0] ALUOp;
-    wire [1:0] RegSrc;
-    wire ALUSrc, RegWrite, MemRead, MemWrite, Branch, Jump;
+    wire [1:0] ALUOp; // EX
+    wire [1:0] RegSrc; // WB
+    wire ALUSrc, RegWrite, MemRead, MemWrite, Branch, Jump; // EX, WB, MEM, MEM, WB, 
     wire [3:0] field;
-    wire zero, sign, overflow, carry;
-    wire branch_taken;
-
-
-    // ================================== DATA FIELDS ===================================
-
-    reg [31:0] rd_write_data;
+    wire [31:0] eximm;
     wire [31:0] rs1_data;
     wire [31:0] rs2_data;
-    wire [31:0] eximm;
-    wire [31:0] op1;
-    wire [31:0] op2;
-    assign op1 = (opcode == 7'b0110111) ? 0: rs1_data;
-    assign op2 = ALUSrc ? eximm: rs2_data;
-    wire [31:0] ALU_result;
 
-
-    // ===================================== MODULES ====================================
+    // TODO: FILL PIPELINE REGS WITH CONTROL SIGNALS, REWRITE MODULE INSTANTIATIONS TO FIT IN EACH STAGE
 
     ControlUnit INST1 (
         .opcode(opcode), 
@@ -103,6 +103,27 @@ module top (
         .regbit(opcode[5]), 
         .field(field)
     );
+    
+
+    // ==================================== EXECUTE =====================================
+
+    wire zero, sign, overflow, carry;
+    wire branch_taken;
+    
+    wire [31:0] EX_pc;
+    wire [31:0] EX_rs1_data;
+    wire [31:0] EX_rs2_data;
+    wire [31:0] EX_eximm;
+    wire [31:0] op1;
+    wire [31:0] op2;
+
+    assign EX_pc = ID/EX[127:96];
+    assign EX_rs1_data = ID/EX[95:64];
+    assign EX_rs2_data = ID/EX[63:32];
+    assign EX_eximm = ID/EX[31:0];
+    assign op1 = (ALUOp == 1 && ALUSrc == 1) ? 0: rs1_data;
+    assign op2 = ALUSrc ? eximm: rs2_data;
+    wire [31:0] ALU_result;
 
     ALU INST5 (
         .op1(op1), 
@@ -124,7 +145,34 @@ module top (
         .funct3(funct3),
         .branch_taken(branch_taken)
     );
+
+
+    // ================================== MEMORY WRITE ==================================
+
+
+    // =============================== REGFILE WRITE BACK ===============================
+
+    reg [31:0] rd_write_data;
     
+
+    always @ (posedge clk or negedge rst_n) begin
+
+        if (!rst_n) begin
+            pc <= 0;
+        end
+
+        else begin
+
+            pc <= next_pc;
+            IF/ID <= {pc,instruction};
+            ID/EX <= {pc, rs1_data, rs2_data, eximm};
+            EX/MEM <= {pc+eximm, rs2_data, alu_result, zero, sign, overflow, carry};
+            MEM/WB <= {alu_result, mem[alu_result]};
+
+        end
+
+    end
+
     always @ (*) begin
 
         instruction = {mem[pc+3], mem[pc+2], mem[pc+1], mem[pc]};
