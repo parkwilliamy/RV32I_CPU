f3a456b6891da732f27ee5e8bdb0554477318e7e
parkwilliamy <parkwilliamy@gmail.com>
2025-11-22 21:17:08 -0500

    compiled tests

--- tb/prog/asm/add_2.s
+++ tb/prog/asm/add_2.s
@@ -1,14 +1,10 @@
     .globl _start
 
+    .section .text
 _start:
-    addi x1, x2, 5          # x1 = x2 + 5
-    addi x3, x4, 8          # x3 = x4 + 8
-    
-
-
-# riscv32-unknown-elf-as -march=rv32i -mabi=ilp32 asm/ADD.s -o obj/add.o
-# riscv32-unknown-elf-ld -T ../../../scripts/link.ld obj/add.o -o elf/add.elf
-# riscv32-unknown-elf-objcopy -O binary elf/add.elf bin/add.bin
-# hexdump -v -e '1/1 "%02x\n"' bin/add.bin > hex/add.hex
-
-
+    addi x1, x0, 1          # x1 = 1
+    addi x2, x0, 2          # x2 = 2
+    addi x3, x0, 3          # x3 = 3
+    addi x4, x0, 4          # x4 = 4
+    addi x5, x0, 5          # x5 = 5
+    addi x6, x0, 6          # x6 = 6
